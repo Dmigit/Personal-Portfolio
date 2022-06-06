17c24b893a3343511ca331068d131c6c599425e5
Dylan Molina <dylanivanmolina@gmail.com>
2022-06-06 09:03:35 -0500

    Fully develop site

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('name').onclick = function() {
+    window.alert('Thank you for visiting my site. It\'s my first personal website and I hope you liked it! I\'ll keep working on it to make it better!');
+}
